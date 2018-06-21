88aadcad2ef3c78c06f4547af35d6a5108b2a2aa
Raphael Jolly <rjolly@users.sourceforge.net>
2018-06-21 22:07:50 +0200

    empty

--- build.js
+++ build.js
@@ -5,6 +5,6 @@ javac("src", "build/classes");
 copy("res", "build/classes");
 
 mkdir("dist");
-jar("dist/l2fprod-common-all.jar", "build/classes", ".*", "manifest.mf");
+jar("dist/l2fprod-common-sheet.jar", "build/classes");
 
 publish("dist")
